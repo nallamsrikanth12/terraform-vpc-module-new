3fcf0fd4101c80f027c2c88d24c05c1f1052d4f7
nallamsrikanth12 <nallamsrikanth3@gmail.com>
2024-06-07 19:51:09 +0900

    new

--- output.tf
+++ output.tf
@@ -8,3 +8,32 @@ output "vpc_id" {
     value = aws_vpc.main.id
   
 }
+
+output "public_subnet_ids" {
+    value = aws_subnet.public_subnet[*].id
+  
+}
+
+output "private_subnet_ids" {
+    value = aws_subnet.private_subnet[*].id
+  
+}
+
+output "database_subnet_ids" {
+    value = aws_subnet.database_subnet[*].id
+  
+}
+
+output "igw_id" {
+    value = aws_internet_gateway.gw.id
+  
+}
+
+output "ngw_id" {
+    value = aws_nat_gateway.ngw.id
+  
+}
+
+output "peering_id" {
+  value = aws_vpc_peering_connection.peering_connection.id
+}
